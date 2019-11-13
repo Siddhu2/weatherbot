4d005de7790961fcce5aee070226def786af7723
Siddharth.M <mksiddhu2@gmail.com>
2019-11-13 11:47:39 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -25,7 +25,7 @@ def makeResponse(req):
 	parameters = result.get("parameters")
 	city = parameters.get("geo-city")
 	date = parameters.get("date")
-	r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22')
+	r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=53b3b494781d9b2bb87099554ba47e44')
 	json_object = r.json()
 	weather=json_object['list']
 	for i in range(0,30):
